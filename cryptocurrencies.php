f81d85591d07f211d492b2c4fbe8247a99635a0d
Sebastian Londero <xebadevs@gmail.com>
2021-12-24 17:04:36 -0300

    'API_settings'

--- cryptocurrencies.php
+++ cryptocurrencies.php
@@ -1,3 +1,37 @@
+<?php
+
+// CoinMarketCap PHP settings
+
+$url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+$parameters = [
+    'start' => '1',
+    'limit' => '1',
+    'convert' => 'USD'
+];
+
+$headers = [
+    'Accepts: application/json',
+//   'Accept-Encoding: deflate, gzip',
+    'X-CMC_PRO_API_KEY: 5edb431f-494d-4212-8525-e42fbcd5428b'
+];
+$qs = http_build_query($parameters); // query string encode the parameters
+$request = "{$url}?{$qs}"; // create the request URL
+
+
+$curl = curl_init(); // Get cURL resource
+// Set cURL options
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $request,            // set the request URL
+    CURLOPT_HTTPHEADER => $headers,     // set the headers
+    CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+));
+
+$response = curl_exec($curl); // Send the request, save the response
+print_r(json_decode($response)); // print json decoded response
+curl_close($curl); // Close request
+
+?>
+
 <section class="section">
         <div class="columns is-centered">
             <div class="column is-2 xd-bshadow">
