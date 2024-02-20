4d911f83931ec226268a18c54a3689cb8b736e72
Sebastián Londero <xebadevs@gmail.com>
2024-02-20 15:50:30 +1000

    fixing news for production

--- news.php
+++ news.php
@@ -1,10 +1,6 @@
 <?php
 
-// ------------------------------ cURL: Multiple Instances ------------------------------ //
-// cURL_init
-
-use function PHPSTORM_META\type;
-
+// cURL Initialization
 $ch_aux = curl_init();
 $ch_new = curl_init();
 
@@ -17,59 +13,30 @@ $apiKeyAux = http_build_query([
 ]);
 $apiKeyNew = 'apiKey=ae1fa2e325e04d0c9e1a93c6988517e0';
 
-// cURL_urls
+// cURL URLs
 $url_aux = sprintf('%s?%s', 'https://api.marketaux.com/v1/news/all', $apiKeyAux);
 $url_new = 'https://newsapi.org/v2/everything?domains=wsj.com&' . $apiKeyNew;
 
-// cURL_set_urls and config
+// cURL Set URLs and Configurations
 curl_setopt($ch_aux, CURLOPT_URL, $url_aux);
 curl_setopt($ch_aux, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch_new, CURLOPT_URL, $url_new);
 curl_setopt($ch_new, CURLOPT_USERAGENT, 'Coinversion/1.0');
 curl_setopt($ch_new, CURLOPT_RETURNTRANSFER, true);
 
-// cURL_multiple_handle
-$mh = curl_multi_init();
+// Execute cURL Requests
+$resp_aux = curl_exec($ch_aux);
+$resp_new = curl_exec($ch_new);
 
-// cURL_insert_handle
-curl_multi_add_handle($mh, $ch_aux);
-curl_multi_add_handle($mh, $ch_new);
+// Close cURL Sessions
+curl_close($ch_aux);
+curl_close($ch_new);
 
-// cURL_do while
-do {
-    $status = curl_multi_exec($mh, $active);
-    if ($active) {
-        curl_multi_select($mh);
-    }
-} while (
-    $active && $status == CURLM_OK
-);
+// Decode JSON Responses
+$dec_aux = json_decode($resp_aux, true);
+$dec_new = json_decode($resp_new, true);
 
-// cURL_close
-curl_multi_remove_handle($mh, $ch_aux);
-curl_multi_remove_handle($mh, $ch_new);
-
-
-// ------------------------------ RESULTS ------------------------------ //
-
-$resp_aux = curl_multi_getcontent($ch_aux);
-$resp_new = curl_multi_getcontent($ch_new);
-
-if ($e = curl_error($ch_aux)) {
-    echo $e;
-} else {
-    $dec_aux = json_decode($resp_aux, true);
-}
-
-if ($e = curl_error($ch_new)) {
-    echo $e;
-} else {
-    $dec_new = json_decode($resp_new, true);
-}
-
-
-// ------------------------------ VARIABLES ------------------------------ //
-
+// Extracting Data
 for ($i = 0; $i < 3; $i++) {
     ${"aux_source$i"} = ucfirst($dec_aux['data'][$i]['source']);
     ${"aux_desc$i"} = $dec_aux['data'][$i]['description'];
@@ -79,18 +46,14 @@ for ($i = 0; $i < 3; $i++) {
     ${"new_desc$i"} = $dec_new['articles'][$i]['description'] ?? $dec_new['articles'][$i]['title'];
 }
 
-
-// ------------------------------ RESULTS FALLBACK ------------------------------ //
-
+// Results Fallback
 if (str_contains($resp_aux, "usage_limit_reached") || str_contains($resp_new, "rateLimited")) {
     $aux_source0 = $aux_source1 = $aux_source2 = $new_source0 = $new_source1 = $new_source2 = "API exceeded";
     $aux_desc0 = $aux_desc1 = $aux_desc2 = $new_desc0 = $new_desc1 = $new_desc2 = "The usage limit for this account has been reached. This site have made too many requests recently. Developer accounts are limited to 100 requests over a 24 hour period (50 requests available every 12 hours).";
     $aux_link0 = $aux_link1 = $aux_link2 = $new_link0 = $new_link1 = $new_link2 = "#";
 }
 
-
-// ------------------------------ DOM HELPER ------------------------------ //
-
+// Helper Function to Generate HTML Boxes
 function generateBox($source, $desc, $link)
 {
     return <<<HTML
@@ -122,8 +85,7 @@ function generateBox($source, $desc, $link)
 
 ?>
 
-<!-- ---------------------------- HTML DOM, CARDS 1 TO 6 ---------------------------- -->
-
+<!-- HTML DOM, CARDS 1 TO 6 -->
 <section class="section">
     <div class="columns is-centered">
         <?php
